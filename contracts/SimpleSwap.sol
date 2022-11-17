d67664f2bea5ab53b4fea8d8aa88324657622631
Jack <jack21.chiang@gmail.com>
2022-11-17 18:32:51 +0800

    a

--- contracts/SimpleSwap.sol
+++ contracts/SimpleSwap.sol
@@ -25,7 +25,38 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
     /// @param tokenOut The address of the token to swap to
     /// @param amountIn The amount of tokenIn to swap
     /// @return amountOut The amount of tokenOut received
-    function swap(address tokenIn, address tokenOut, uint256 amountIn) external returns (uint256 amountOut) {}
+    function swap(address tokenIn, address tokenOut, uint256 amountIn) external returns (uint256 amountOut) {
+        // 檢查
+        // console.log("%s vs %s", tokenA, tokenIn);
+        require(tokenIn == tokenA || tokenIn == tokenB, "SimpleSwap: INVALID_TOKEN_IN");
+        require(tokenOut == tokenA || tokenOut == tokenB, "SimpleSwap: INVALID_TOKEN_OUT");
+        require(tokenIn != tokenOut, "SimpleSwap: IDENTICAL_ADDRESS");
+        require(amountIn > 0, "SimpleSwap: INSUFFICIENT_INPUT_AMOUNT");
+        // 計算
+        (uint256 _reserveA, uint256 _reserveB) = this.getReserves();
+        uint256 _k = _reserveA * _reserveB;
+        uint256 _amountOut = 0;
+        // tokenIn == tokenA
+        // ? _reserveB - _k / (_reserveA + amountIn)
+        // : _reserveA - _k / (_reserveB + amountIn);
+        if (tokenIn == tokenA) {
+            _amountOut = _reserveB - _k / (_reserveA + amountIn);
+            reserveA += amountIn;
+            reserveB -= _amountOut;
+        } else {
+            _amountOut = _reserveA - _k / (_reserveB + amountIn);
+            reserveA -= _amountOut;
+            reserveB += amountIn;
+        }
+        // console.log("K: %s, Out: %s, T: %s", _k, _amountOut, _k / (_reserveA + amountIn));
+        require(_amountOut > 0, "SimpleSwap: INSUFFICIENT_OUTPUT_AMOUNT");
+        // 轉帳
+        ERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn);
+        ERC20(tokenOut).approve(address(this), _amountOut);
+        ERC20(tokenOut).transferFrom(address(this), msg.sender, _amountOut);
+        emit Swap(msg.sender, tokenIn, tokenOut, amountIn, _amountOut);
+        return _amountOut;
+    }
 
     /// @notice Add liquidity to the pool
     /// @param amountAIn The amount of tokenA to add
@@ -37,42 +68,74 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
         uint256 amountAIn,
         uint256 amountBIn
     ) external returns (uint256 amountA, uint256 amountB, uint256 liquidity) {
-        console.log("A: %s, B: %s", amountA, amountB);
         // 檢查
-        require(amountA > 0 && amountB > 0, "SimpleSwap: INSUFFICIENT_INPUT_AMOUNT");
-        (uint256 reserveA, uint256 reserveB) = this.getReserves();
-        uint256 liquidity = Math.sqrt(amountA * amountB);
-        ERC20(tokenA).transfer(address(this), amountA);
-        ERC20(tokenB).transfer(address(this), amountB);
-        transfer(msg.sender, liquidity);
-        reserveA += amountA;
-        reserveB += amountB;
-        emit AddLiquidity(msg.sender, amountA, amountB, liquidity);
-        return (amountA, amountB, liquidity);
+        require(amountAIn > 0 && amountBIn > 0, "SimpleSwap: INSUFFICIENT_INPUT_AMOUNT");
+        // 計算
+        uint256 _amountAIn = 0;
+        uint256 _amountBIn = 0;
+        uint256 _liquidity = 0;
+        if (totalSupply() == 0) {
+            _amountAIn = amountAIn;
+            _amountBIn = amountBIn;
+            _liquidity = Math.sqrt(_amountAIn * _amountBIn);
+            // uint256 balanceA = ERC20(tokenA).balanceOf(msg.sender);
+            // uint256 balanceB = ERC20(tokenB).balanceOf(msg.sender);
+            // console.log("1 A: %s, B: %s", amountAIn, amountBIn);
+            // console.log("2 A: %s, B: %s", balanceA, balanceB);
+        } else {
+            (uint256 _reserveA, uint256 _reserveB) = this.getReserves();
+            _amountAIn = Math.min(amountAIn, (amountBIn * _reserveA) / _reserveB);
+            _amountBIn = Math.min(amountBIn, (amountAIn * _reserveB) / _reserveA);
+            _liquidity = Math.sqrt(_amountAIn * _amountBIn);
+        }
+        // 轉帳
+        ERC20(tokenA).transferFrom(msg.sender, (address(this)), _amountAIn);
+        ERC20(tokenB).transferFrom(msg.sender, (address(this)), _amountBIn);
+        _mint(msg.sender, _liquidity);
+        reserveA += _amountAIn;
+        reserveB += _amountBIn;
+        // console.log("3 A: %s, B: %s", reserveA, reserveB);
+        emit AddLiquidity(msg.sender, _amountAIn, _amountBIn, _liquidity);
+        return (_amountAIn, _amountBIn, _liquidity);
     }
 
     /// @notice Remove liquidity from the pool
     /// @param liquidity The amount of liquidity to remove
     /// @return amountA The amount of tokenA received
     /// @return amountB The amount of tokenB received
-    function removeLiquidity(uint256 liquidity) external returns (uint256 amountA, uint256 amountB) {}
+    function removeLiquidity(uint256 liquidity) external returns (uint256 amountA, uint256 amountB) {
+        // 檢查
+        require(liquidity > 0, "SimpleSwap: INSUFFICIENT_LIQUIDITY_BURNED");
+        // 計算
+        uint256 _totalSupply = totalSupply();
+        (uint256 _reserveA, uint256 _reserveB) = this.getReserves();
+        amountA = (liquidity * _reserveA) / _totalSupply;
+        amountB = (liquidity * _reserveB) / _totalSupply;
+        // 轉帳
+        ERC20(tokenA).transfer(msg.sender, amountA);
+        ERC20(tokenB).transfer(msg.sender, amountB);
+        _burn(msg.sender, liquidity);
+        reserveA -= amountA;
+        reserveB -= amountB;
+        emit RemoveLiquidity(msg.sender, amountA, amountB, liquidity);
+    }
 
     /// @notice Get the reserves of the pool
-    /// @return reserveA The reserve of tokenA
-    /// @return reserveB The reserve of tokenB
-    function getReserves() external view returns (uint256 reserveA, uint256 reserveB) {
+    /// @return _reserveA The reserve of tokenA
+    /// @return _reserveB The reserve of tokenB
+    function getReserves() external view returns (uint256 _reserveA, uint256 _reserveB) {
         return (reserveA, reserveB);
     }
 
     /// @notice Get the address of tokenA
-    /// @return tokenA The address of tokenA
-    function getTokenA() external view returns (address tokenA) {
+    /// @return _tokenA The address of tokenA
+    function getTokenA() external view returns (address _tokenA) {
         return tokenA;
     }
 
     /// @notice Get the address of tokenB
-    /// @return tokenB The address of tokenB
-    function getTokenB() external view returns (address tokenB) {
+    /// @return _tokenB The address of tokenB
+    function getTokenB() external view returns (address _tokenB) {
         return tokenB;
     }
 }
